5f3e01b2c493ebb334e1916c72036dffef9fb4bf
Wesley Wang <wangdeyi427@gmail.com>
2021-11-14 20:45:13 +0800

    new video

--- src/components/Invitation.vue
+++ src/components/Invitation.vue
@@ -6,7 +6,7 @@
           <div class="content-inside">
             <!-- 拆信音效 -->
             <audio id="envelope" :src="require('../audios/envelope.mp3')"></audio>
-            <video class="content-inside-photo" id="video" :src="require('../videos/test.mp4')"
+            <video class="content-inside-photo" id="video" :src="require('../videos/video.mp4')"
                    controls="true" controlslist="nodownload noplaybackrate" disablePictureInPicture="true"></video>
             <p>我们结婚啦！</p>
             <p><b>王得屹 & 张楠鸽</b></p>
